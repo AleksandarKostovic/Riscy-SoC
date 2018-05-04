15d605a52799f0bfca15d9588e51ff9cbecc6388
AleksandarKostovic <32797225+AleksandarKostovic@users.noreply.github.com>
2018-05-04 15:31:22 +0200

    Update ram.sv

--- ram.sv
+++ ram.sv
@@ -1,1 +1,17 @@
-//ram
+module sp_ram (
+  input [5:0] addr,
+  input [7:0] data,
+  input we,clk,
+  output [7:0] q
+);
+  reg [5:0] addr_reg;
+  reg [7:0] ram [63:0];
+  always @ (posedge clk)
+  begin  
+    if (we)
+      ram[addr] <= data;
+      addr_reg <= addr;
+  end
+  
+  assign q = ram[addr_reg];
+endmodule   
