f5a6b1fa8c2590244bf328ad17eeac4045639aa0
AleksandarKostovic <32797225+AleksandarKostovic@users.noreply.github.com>
2018-05-15 15:36:17 +0200

    Update cu.sv

--- rtl/cpu/cu.sv
+++ rtl/cpu/cu.sv
@@ -10,28 +10,28 @@ module control_unit (
     output logic rs2_read_out,
     output logic [4:0] imm_out,
     output logic [4:0] alu_op_out,
-    output logic alu_sub_sra_out,
+    output logic [2:0]alu_sub_sra_out,
     output logic [2:0] alu_src1_out,
     output logic [2:0] alu_src2_out,
     output logic mem_read_out,
     output logic mem_write_out,
     output logic [2:0] mem_width_out,
-    output logic mem_zero_extend_out,
+    output logic [2:0] mem_zero_extend_out,
     output logic mem_fence_out,
     output logic csr_read_out,
     output logic csr_write_out,
     output logic [2:0] csr_write_op_out,
-    output logic csr_src_out,
+    output logic [2:0]csr_src_out,
     output logic [2:0] branch_op_out,
-    output logic branch_pc_src_out,
+    output logic [2:0] branch_pc_src_out,
     output logic rd_write_out
 );
     always_comb begin
         valid_out = 0;
         rs1_read_out = 0;
         rs2_read_out = 0;
-        imm_out = 7'bx;
-        alu_op_out = 7'bx;
+        imm_out = 8'bx;
+        alu_op_out = 8'bx;
         alu_sub_sra_out = 2'bx;
         alu_src1_out = 4'bx;
         alu_src2_out = 2'bx;
