5fabd943461483af45ef285d8ac8c5b8011f69af
AleksandarKostovic <32797225+AleksandarKostovic@users.noreply.github.com>
2018-05-22 16:22:49 +0200

    Update opcodes.sv

--- rtl/cpu/opcodes.sv
+++ rtl/cpu/opcodes.sv
@@ -4,33 +4,21 @@
 `define STORE    7'b0100011
 `define MADD     7'b1000011
 `define BRANCH   7'b1100011
-
 `define LOAD_FP  7'b0000111
 `define STORE_FP 7'b0100111 
 `define MSUB     7'b1000111
 `define JALR     7'b1100111
-
-`define CUSTOM_0 7'b0001011
-`define CUSTOM_1 7'b0101011
 `define NMSUB    7'b1001011
-
-
 `define MISC_MEM 7'b0001111
 `define AMO      7'b0101111
 `define NMADD    7'b1001111
 `define JAL      7'b1101111
-
 `define OP_IMM   7'b0010011
 `define OP       7'b0110011
 `define OP_FP    7'b1010011
 `define SYSTEM   7'b1110011
-
 `define AUIPC    7'b0010111
 `define LUI      7'b0110111
-
-`define CUSTOM_2 7'b1011011
-`define CUSTOM_3 7'b1111011
-
 
 `define FUNCT3_ADD_SUB 0
 `define FUNCT3_SLL     1
