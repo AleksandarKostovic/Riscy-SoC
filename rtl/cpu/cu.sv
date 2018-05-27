7288d787bbff0b71a15ac0a05bc28e9c0d78570c
AleksandarKostovic <32797225+AleksandarKostovic@users.noreply.github.com>
2018-05-27 19:12:53 +0200

    Update cu.sv

--- rtl/cpu/cu.sv
+++ rtl/cpu/cu.sv
@@ -5,28 +5,28 @@ module control_unit (
     input [8:0] rs1_in,
     input [8:0] rd_in,
 
-    output logic valid_out,
-    output logic rs1_read_out,
-    output logic rs2_read_out,
-    output logic [4:0] imm_out,
-    output logic [4:0] alu_op_out,
-    output logic [2:0]alu_sub_sra_out,
-    output logic [2:0] alu_src1_out,
-    output logic [2:0] alu_src2_out,
-    output logic mem_read_out,
-    output logic mem_write_out,
-    output logic [2:0] mem_width_out,
-    output logic [2:0] mem_zero_extend_out,
-    output logic mem_fence_out,
-    output logic csr_read_out,
-    output logic csr_write_out,
-    output logic [2:0] csr_write_op_out,
-    output logic [2:0]csr_src_out,
-    output logic [2:0] branch_op_out,
-    output logic [2:0] branch_pc_src_out,
-    output logic rd_write_out
+    output valid_out,
+    output rs1_read_out,
+    output rs2_read_out,
+    output [4:0] imm_out,
+    output [4:0] alu_op_out,
+    output [2:0]alu_sub_sra_out,
+    output [2:0] alu_src1_out,
+    output [2:0] alu_src2_out,
+    output mem_read_out,
+    output mem_write_out,
+    output [2:0] mem_width_out,
+    output [2:0] mem_zero_extend_out,
+    output mem_fence_out,
+    output csr_read_out,
+    output csr_write_out,
+    output [2:0] csr_write_op_out,
+    output [2:0]csr_src_out,
+    output [2:0] branch_op_out,
+    output [2:0] branch_pc_src_out,
+    output rd_write_out
 );
-    always_comb begin
+    always@(*) begin
         valid_out = 0;
         rs1_read_out = 0;
         rs2_read_out = 0;
